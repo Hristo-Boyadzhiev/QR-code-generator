76f73d25448201683676f4931f2c20196a67efdc
Hristo Boyadzhiev <hristo_boyadzhiev@abv.bg>
2024-10-22 08:28:06 +0300

    Update encryption Types

--- QR-code-generator/src/enums/EncryptionTypes.ts
+++ QR-code-generator/src/enums/EncryptionTypes.ts
@@ -1,7 +1,9 @@
 export enum encryptionTypes {
   None = "None",
-  WPA = "WPA",
-  WPA2 = "WPA2",
-  WPA3 = "WPA3",
   WEP = "WEP",
+  WPA = "WPA",
+  WPA2_WPA3 = "WPA2/WPA3",
+  WPA_Enterprise = "WPA Enterprise",
+  WPA2_Enterprise = "WPA2 Enterprise",
+  WPA3_Enterprise = "WPA3 Enterprise",
 }
